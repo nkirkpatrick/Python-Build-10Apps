5c89ad08c0756ce57c20735defe7e9f2bec9cf93
Norbert Kirkpatrick <norbertkirkpatrick@norberts-mbp.lan>
2021-02-07 14:24:06 -0800

    Initial setup for Real Estate App

---
+++ App9-real-estate-analysis/program.py
@@ -0,0 +1,15 @@
+
+def main():
+    print_header()
+
+def print_header():
+    print('---------------------------------------------')
+    print(' REAL ESTATE DATA MINING APP')
+    print('---------------------------------------------')
+
+def get_data_file():
+    pass
+
+if __name__ == '__main__':
+    main()
+
